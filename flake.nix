a06e8ec47caecfda1082750d971d81e740c69109
0xWal <imwaleed@outlook.sa>
2025-10-14 09:07:17 +0300

    chore(flake): add flake for nix :wrench:

---
+++ flake.nix
@@ -0,0 +1,146 @@
+# vim: set ts=2 sw=2 et:
+{
+  description = "Niri scripts";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable = {
+      # NOTE: WE NEED CARGO 1.88+ AS IT HAS rust#132833
+      # NOW STABLE NIX HAS ONLY CARGO 1.86
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      unstablePkgs = nixpkgs-unstable.legacyPackages.${system};
+
+      scriptisto = pkgs.scriptisto;
+
+      # helper to wrap an rs script
+      wrapRs =
+        scriptFile: name:
+        pkgs.stdenv.mkDerivation rec {
+          pname = builtins.baseNameOf scriptFile; # name by file name
+          version = "0.1.0";
+
+          src = scriptFile;
+          dontUnpack = true;
+
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          buildInputs = [ pkgs.scriptisto ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            # copy script into output
+            cp $src $out/${pname}.rs
+            # wrap
+            makeWrapper ${scriptisto}/bin/scriptisto $out/bin/${name} \
+              --add-flags "$out/${pname}.rs" \
+              --prefix PATH : ${
+                pkgs.lib.makeBinPath [
+                  unstablePkgs.rustc
+                  unstablePkgs.cargo
+                  unstablePkgs.rustPackages.clippy
+                  pkgs.gcc
+                  pkgs.pkg-config
+                ]
+              }
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Niri scripts";
+            homepage = "https://github.com/0xWal/niri-scripts";
+            license = licenses.mit;
+            platforms = platforms.linux;
+            mainProgram = name;
+          };
+        };
+
+      wallpaper = wrapRs ./wallpaper-per-workspace "niri-wpw";
+      supportSticky = wrapRs ./support-sticky-floating "niri-support-sticky";
+      screenshot = wrapRs ./screenshot "niri-screenshot";
+
+    in
+    {
+      packages.${system} = {
+        supportSticky = supportSticky;
+        wallpaper = wallpaper;
+        screenshot = screenshot;
+      };
+
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        system = system;
+        modules = [
+          (import "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-base.nix")
+          {
+            imports = [
+              self.nixosModules.default
+
+            ];
+
+            users.users.niri = {
+              isNormalUser = true;
+              description = "Niri Test User";
+              extraGroups = [ "wheel" ]; # for sudo
+            };
+
+            services.getty.autologinUser = pkgs.lib.mkForce "niri";
+            environment.systemPackages = [
+              supportSticky
+              wallpaper
+              screenshot
+            ];
+          }
+          {
+            hardware.graphics.enable = true;
+            programs.niri.enable = true;
+            niri-scripts = {
+              enable = true;
+              screenshot = {
+                enable = true;
+                dir = "~/Pictures/Screenshots";
+              };
+              wallpaper-per-workspace = {
+                enable = true;
+                dir = "~/.wallpapers";
+              };
+              sticky-window.enable = true;
+            };
+          }
+        ];
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          supportSticky
+          wallpaper
+          screenshot
+        ];
+      };
+
+      # nixosModules.default = (
+      #   import ./nix/niri-scripts.nix (with pkgs; {
+      #     inherit
+      #       config
+      #       lib
+      #       pkgs
+      #       ;
+      #     niriScriptsPkg = {
+      #       inherit supportSticky wallpaper screenshot;
+      #     };
+      #   })
+      # );
+
+      nixosModules.default = import ./nix/niri-scripts.nix { inherit self; };
+      # nixosModules.default = ./nix/niri-scripts.nix;
+    };
+}
---
+++ nix/niri-scripts.nix
@@ -0,0 +1,96 @@
+# vim: set ts=2 sw=2 et:
+{ self }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+let
+  cfg = config.niri-scripts;
+  selfPkgs = self.packages.${pkgs.system};
+in
+{
+  options.niri-scripts = {
+    enable = mkEnableOption "Enable all Niri scripts";
+
+    screenshot = {
+      enable = mkEnableOption "Enable screenshot script";
+      dir = mkOption {
+        type = types.str;
+        default = "~/Screenshots";
+        description = "Directory to save screenshots to";
+      };
+    };
+
+    sticky-window = {
+      enable = mkEnableOption "Enable sticky window support";
+    };
+
+    wallpaper-per-workspace = {
+      enable = mkEnableOption "Enable wallpaper-per-workspace script";
+      dir = mkOption {
+        type = types.str;
+        default = "~/wallpapers";
+        description = "Directory containing wallpapers per workspace";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+
+    (mkIf cfg.screenshot.enable {
+      assertions = [
+        {
+          assertion = config.programs.niri.enable or false;
+          message = "niri-scripts requires programs.niri.enable = true";
+        }
+      ];
+
+      environment.systemPackages = [
+        selfPkgs.screenshot
+        pkgs.scriptisto
+        pkgs.grim
+        pkgs.satty
+        pkgs.slurp
+        pkgs.wl-clipboard
+      ];
+    })
+
+    (mkIf cfg.sticky-window.enable {
+      environment.systemPackages = [ selfPkgs.supportSticky ];
+
+      # systemd.user.services.support-sticky-floating = {
+      #   enable = true;
+      #   description = "Niri sticky window support";
+      #   serviceConfig = {
+      #     ExecStart = "${lib.getExe selfPkgs.supportSticky}";
+      #     After = "niri.service";
+      #     Requires = "niri.service";
+      #   };
+      #   wantedBy = [ "default.target" ];
+      # };
+    })
+
+    (mkIf cfg.wallpaper-per-workspace.enable {
+      environment.systemPackages = [
+        selfPkgs.wallpaper
+        pkgs.swww
+      ];
+
+      # systemd.user.services.wallpaper-per-workspace = {
+      #   enable = true;
+      #   description = "Niri wallpaper per workspace";
+      #   serviceConfig = {
+      #     ExecStart = "${lib.getExe selfPkgs.wallpaper} ${cfg.wallpaper-per-workspace.dir}";
+      #     Restart = "always";
+      #     After = "niri.service";
+      #     Requires = "niri.service";
+      #   };
+      #   wantedBy = [ "default.target" ];
+      # };
+    })
+  ]);
+}
